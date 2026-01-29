453188bfb970cef2e037574412b8c1e0211b695e
Евгений <evgkostovar@gmail.com>
2026-01-29 21:43:26 +0300

    д/з к уроку 4

---
+++ js/modal.js
@@ -0,0 +1,23 @@
+let modalButton = document.querySelector('.more');
+const modalButtons = document.querySelectorAll('.more');
+const modal = document.querySelector('.modal');
+const overlay = modal.querySelector('.overlay');
+const closeButton = modal.querySelector('.modal__close');
+
+/* modalButton.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+}) */
+
+overlay.addEventListener('click', () => {
+    modal.classList.add('hidden');
+})
+
+closeButton.addEventListener('click', () => {
+    modal.classList.add('hidden');
+})
+
+modalButtons.forEach((modalButton) => {
+    modalButton.addEventListener('click', () => {
+        modal.classList.remove('hidden');
+    })
+}) 
